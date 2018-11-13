f3fd9c999386970025a451170f2e0fc065f04c72
Henry Richardson <hrichardson83@gmail.com>
2018-11-13 13:40:51 -0500

    adding in routing with transition animation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Routes from "./routes";
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Routes />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
